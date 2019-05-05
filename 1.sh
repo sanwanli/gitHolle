77ab6742cb703f3643f44cc5f380d89f6c1afc16
root <puyi@168cad.top>
2019-05-05 17:36:20 +0800

    pwd

--- 1.sh
+++ 1.sh
@@ -7,6 +7,17 @@ echo "加法"
 
 sum=`expr $1 + $2`
 echo "$1 + $2 = $sum"
+<<<<<<< Updated upstream
 loading....
 
 123
+=======
+
+
+echo "loading............."
+
+th=$(pwd)
+
+echo -e "当前路径是:$th"
+
+>>>>>>> Stashed changes
